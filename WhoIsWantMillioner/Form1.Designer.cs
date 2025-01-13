ce83d1fdacd7988d860e80283868d1a83c0a8894
Matvey1Tomilov <matyusha.tomilov@bk.ru>
2025-01-13 13:38:04 +0500

    Добавьте файлы проекта.

---
+++ WhoIsWantMillioner/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace WhoIsWantMillioner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnAnswerA = new System.Windows.Forms.Button();
+            this.btnAnswerB = new System.Windows.Forms.Button();
+            this.btnAnswerC = new System.Windows.Forms.Button();
+            this.btnAnswerD = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lstLevel = new System.Windows.Forms.ListBox();
+            this.FiftyFiftyButton = new System.Windows.Forms.Button();
+            this.HelpPeopleButton = new System.Windows.Forms.Button();
+            this.ErrorButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WhoIsWantMillioner.Properties.Resources.maxresdefault2;
+            this.pictureBox1.Location = new System.Drawing.Point(454, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(647, 381);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnAnswerA
+            // 
+            this.btnAnswerA.Location = new System.Drawing.Point(454, 593);
+            this.btnAnswerA.Name = "btnAnswerA";
+            this.btnAnswerA.Size = new System.Drawing.Size(200, 50);
+            this.btnAnswerA.TabIndex = 1;
+            this.btnAnswerA.Tag = "1";
+            this.btnAnswerA.Text = "button1";
+            this.btnAnswerA.UseVisualStyleBackColor = true;
+            this.btnAnswerA.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // btnAnswerB
+            // 
+            this.btnAnswerB.Location = new System.Drawing.Point(454, 707);
+            this.btnAnswerB.Name = "btnAnswerB";
+            this.btnAnswerB.Size = new System.Drawing.Size(200, 50);
+            this.btnAnswerB.TabIndex = 2;
+            this.btnAnswerB.Tag = "2";
+            this.btnAnswerB.Text = "button2";
+            this.btnAnswerB.UseVisualStyleBackColor = true;
+            this.btnAnswerB.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // btnAnswerC
+            // 
+            this.btnAnswerC.Location = new System.Drawing.Point(901, 593);
+            this.btnAnswerC.Name = "btnAnswerC";
+            this.btnAnswerC.Size = new System.Drawing.Size(200, 50);
+            this.btnAnswerC.TabIndex = 3;
+            this.btnAnswerC.Tag = "3";
+            this.btnAnswerC.Text = "button3";
+            this.btnAnswerC.UseVisualStyleBackColor = true;
+            this.btnAnswerC.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // btnAnswerD
+            // 
+            this.btnAnswerD.Location = new System.Drawing.Point(901, 707);
+            this.btnAnswerD.Name = "btnAnswerD";
+            this.btnAnswerD.Size = new System.Drawing.Size(200, 50);
+            this.btnAnswerD.TabIndex = 4;
+            this.btnAnswerD.Tag = "4";
+            this.btnAnswerD.Text = "button4";
+            this.btnAnswerD.UseVisualStyleBackColor = true;
+            this.btnAnswerD.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblQuestion.Location = new System.Drawing.Point(451, 479);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(1000, 97);
+            this.lblQuestion.TabIndex = 5;
+            this.lblQuestion.Text = "label1";
+            // 
+            // lstLevel
+            // 
+            this.lstLevel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lstLevel.FormattingEnabled = true;
+            this.lstLevel.ItemHeight = 23;
+            this.lstLevel.Items.AddRange(new object[] {
+            "3 000 000",
+            "1 500 000",
+            "   800 000",
+            "   400 000",
+            "   200 000",
+            "   100 000",
+            "     50 000",
+            "     25 000",
+            "     15 000",
+            "     10 000 ",
+            "       5 000 ",
+            "       3 000 ",
+            "       2 000 ",
+            "       1 000 ",
+            "          500"});
+            this.lstLevel.Location = new System.Drawing.Point(1192, 104);
+            this.lstLevel.Name = "lstLevel";
+            this.lstLevel.Size = new System.Drawing.Size(103, 372);
+            this.lstLevel.TabIndex = 6;
+            this.lstLevel.Click += new System.EventHandler(this.btnMoney);
+            // 
+            // FiftyFiftyButton
+            // 
+            this.FiftyFiftyButton.Location = new System.Drawing.Point(143, 104);
+            this.FiftyFiftyButton.Name = "FiftyFiftyButton";
+            this.FiftyFiftyButton.Size = new System.Drawing.Size(152, 52);
+            this.FiftyFiftyButton.TabIndex = 7;
+            this.FiftyFiftyButton.Tag = "4";
+            this.FiftyFiftyButton.Text = "50/50";
+            this.FiftyFiftyButton.UseVisualStyleBackColor = true;
+            this.FiftyFiftyButton.Click += new System.EventHandler(this.btnHelps_Click);
+            // 
+            // HelpPeopleButton
+            // 
+            this.HelpPeopleButton.Location = new System.Drawing.Point(143, 172);
+            this.HelpPeopleButton.Name = "HelpPeopleButton";
+            this.HelpPeopleButton.Size = new System.Drawing.Size(152, 52);
+            this.HelpPeopleButton.TabIndex = 9;
+            this.HelpPeopleButton.Tag = "5";
+            this.HelpPeopleButton.Text = "Помощь зала";
+            this.HelpPeopleButton.UseVisualStyleBackColor = true;
+            this.HelpPeopleButton.Click += new System.EventHandler(this.btnHelps_Click);
+            // 
+            // ErrorButton
+            // 
+            this.ErrorButton.Location = new System.Drawing.Point(143, 240);
+            this.ErrorButton.Name = "ErrorButton";
+            this.ErrorButton.Size = new System.Drawing.Size(152, 52);
+            this.ErrorButton.TabIndex = 10;
+            this.ErrorButton.Tag = "6";
+            this.ErrorButton.Text = "Право на ошибку";
+            this.ErrorButton.UseVisualStyleBackColor = true;
+            this.ErrorButton.Click += new System.EventHandler(this.btnHelps_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 50);
+            this.button1.TabIndex = 11;
+            this.button1.Tag = "7";
+            this.button1.Text = "Звонок другу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnHelps_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(425, 602);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 27);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(872, 602);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 27);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(425, 716);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 27);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "c";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(872, 716);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 27);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "d";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 953);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ErrorButton);
+            this.Controls.Add(this.HelpPeopleButton);
+            this.Controls.Add(this.FiftyFiftyButton);
+            this.Controls.Add(this.lstLevel);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnAnswerD);
+            this.Controls.Add(this.btnAnswerC);
+            this.Controls.Add(this.btnAnswerB);
+            this.Controls.Add(this.btnAnswerA);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnAnswerA;
+        private System.Windows.Forms.Button btnAnswerB;
+        private System.Windows.Forms.Button btnAnswerC;
+        private System.Windows.Forms.Button btnAnswerD;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.ListBox lstLevel;
+        private System.Windows.Forms.Button FiftyFiftyButton;
+        private System.Windows.Forms.Button HelpPeopleButton;
+        private System.Windows.Forms.Button ErrorButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WhoIsWantMillioner/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhoIsWantMillioner
+{
+    public partial class Form1 : Form
+    {
+        ///SortedList<int, int> Money = new SortedList<int, int>();
+        List<Question> questions = new List<Question>();
+        private Random rnd = new Random();
+        int level = 0;
+        int Money;
+        int MoneyLevel;
+        Question currentQuestion;
+        bool FiftyFifty = true;
+        bool MaybeError = true;
+        bool HelpX2 = false;
+        bool PhoneFriend = true;
+        bool HelpPeople = true;
+        public Form1()
+        {
+            InitializeComponent();
+           /*ReadFile();*/
+            startGame();
+        }
+        private void ShowQuestion(Question q)
+        {
+            lblQuestion.Text = q.Text;
+            btnAnswerA.Text = q.Answers[0];
+            btnAnswerB.Text = q.Answers[1];
+            btnAnswerC.Text = q.Answers[2];
+            btnAnswerD.Text = q.Answers[3];
+        }
+        /*private SortedList<int, int> AddsMoney(SortedList<int,int> Money)
+        {
+            for (int i=0; i < 15 ;i++)
+            {
+                Money.Add(i + 1, lstLevel.);
+            }
+        }*/
+        private void ShowQuestion(Question q,int help)//50/50
+        {
+            lblQuestion.Text = q.Text;
+            if (help == 4)
+            {
+                if (q.RightAnswer == 1)//a
+                {
+                    btnAnswerB.Text = "";
+                    btnAnswerC.Text = "";
+                }
+                else if (q.RightAnswer == 3)//c
+                {
+                    btnAnswerD.Text = "";
+                    btnAnswerA.Text = "";
+                }
+                else if (q.RightAnswer == 2 )//b
+                {
+                    btnAnswerC.Text = "";
+                    btnAnswerD.Text = "";
+                }
+                else if (q.RightAnswer == 4 )//d
+                {
+                    btnAnswerA.Text = "";
+                    btnAnswerB.Text = "";
+                }
+                FiftyFifty = false;
+            }
+        }
+        private Question GetQuestion(int level)//бд
+        {
+            SQLiteConnection cn = new SQLiteConnection();
+            cn.ConnectionString = @"Data Source=WhoWantsToBeAMillionaire.db;Version=3";
+
+            cn.Open();
+
+            var cmd = new SQLiteCommand($@"select * from Questions WHERE Level={level} 
+                                            order by Random() LIMIT 1", cn);
+
+            var dr = cmd.ExecuteReader();
+            dr.Read();
+            Question q = new Question(dr);
+
+            return q;
+        }
+
+        private void NextStep()
+        {
+            Button[] btns = new Button[] { btnAnswerA, btnAnswerB,btnAnswerC, btnAnswerC };
+            foreach (Button btn in btns)
+                btn.Enabled = true;
+
+            level++;
+            currentQuestion = GetQuestion(level);
+            ShowQuestion(currentQuestion);
+            lstLevel.SelectedIndex = lstLevel.Items.Count - level;
+        }
+        private void startGame()
+        {
+            level = 0;
+            MessageBox.Show("Выберите несгораемую сумму:");
+            NextStep();
+        }
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (currentQuestion.RightAnswer == int.Parse(button.Tag.ToString()))
+            {
+                if (level == 15)
+                {
+                    MessageBox.Show("Поздравляем миллионера! Вы выиграли " + Money + " рублей! ");
+                    Application.Exit();
+                }
+                if (HelpX2)
+                {
+                    button.Text = "";
+                    HelpX2 = false;
+                    MaybeError = false;
+                    NextStep(); 
+                }
+                NextStep();
+            }
+            else if (HelpX2)
+            {
+
+                button.Text = "";
+                HelpX2 = false;
+                MaybeError = false;
+            }
+            else
+            {
+                MessageBox.Show("Неверный ответ!Верный ответ: "+ currentQuestion.RightAnswer.ToString());
+                if (MoneyLevel > Convert.ToInt32(lstLevel.SelectedIndex.ToString().Replace(" ","")))
+                {
+                    MessageBox.Show("Поздравляем! Вы выиграли "+Money+" рублей!");
+                }
+
+                Application.Exit();
+               // startGame();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHelps_Click(object sender, EventArgs e)//подсказки
+        {
+            Button button = (Button)sender;
+            if (int.Parse(button.Tag.ToString()) == 4 && FiftyFifty)
+            {
+                ShowQuestion(currentQuestion, 4);
+                button.Text = "Потрачено";
+            }
+            if (int.Parse(button.Tag.ToString()) == 6 && MaybeError)
+            {
+                HelpX2 = true;
+                MaybeError = false;
+                button.Text = "Потрачено";
+            }
+            if (int.Parse(button.Tag.ToString()) == 5 && HelpPeople)
+            {
+                Random rnd = new Random();
+                int first,second,third,fourth;
+                string Answer="";
+       
+                first = rnd.Next(53,90) - level * 2;//34
+                second = rnd.Next(0, 100 - first) / 2;//33
+                third = (100 - first - second )/ 2;//14
+                fourth = 100 - first - second - third;
+                if(currentQuestion.RightAnswer==1)
+                {
+                    Answer = "а - "+first+"%"+" b - "+second+"%"+ "\nc - " + third + "%" + " d - " + fourth + "%";
+                }
+                else if (currentQuestion.RightAnswer == 3)
+                {
+                    Answer = "а - " + second + "%" + " b - " + first + "%" + "\nc - " + third + "%" + " d - " + fourth + "%";
+                }
+                else if (currentQuestion.RightAnswer == 2)
+                {
+                    Answer = "а - " + third + "%" + " b - " + second + "%" + "\nc - " + first + "%" + " d - " + fourth + "%";
+                }
+                else if (currentQuestion.RightAnswer == 4)
+                {
+                    Answer = "а - " + fourth + "%" + " b - " + second + "%" + "\nc - " + third + "%" + " d - " + first + "%";
+                }
+                MessageBox.Show(Answer);
+                button.Text = "Потрачено";
+                HelpPeople = false;
+            }
+            if (int.Parse(button.Tag.ToString()) == 7 && PhoneFriend)
+            {
+                Random rnd = new Random();
+                int first;
+                string Answer = "";
+                first = rnd.Next(64, 95) - level * 2;
+                if (currentQuestion.RightAnswer == 1)
+                {
+                    if (first > 50) { Answer = "Думаю, ответ - a"; }
+                    else { Answer = "Думаю, ответ - b"; }
+                }
+                else if (currentQuestion.RightAnswer == 3)
+                {
+                    if (first > 50) { Answer = "Думаю, ответ - b"; }
+                    else { Answer = "Думаю, ответ - c"; }
+                }
+                else if (currentQuestion.RightAnswer == 2)
+                {
+                    if (first > 50) { Answer = "Думаю, ответ - c"; }
+                    else { Answer = "Думаю, ответ - d"; }
+                }
+                else if (currentQuestion.RightAnswer == 4)
+                {
+                    if (first > 50) { Answer = "Думаю, ответ - d"; }
+                    else { Answer = "Думаю, ответ - a"; }
+                }
+                MessageBox.Show(Answer);
+                PhoneFriend = false;
+                button.Text = "Потрачено";
+            }
+        }
+
+        private void btnMoney(object sender, EventArgs e)
+        {
+            ListBox listbox = (ListBox)sender;
+            string money = listbox.SelectedItem.ToString();
+            string moneylevel = listbox.SelectedIndex.ToString();
+            money= money.Replace(" ","");
+            Money = Convert.ToInt32(money);
+            MoneyLevel = Convert.ToInt32(moneylevel);
+            MessageBox.Show("Вы выбрали "+Money+" рублей ");
+        }
+    }
+}
